c7403fcb41ad44f3d7e24e4aad6e539cd4102b0c
Your Name <you@example.com>
2025-05-15 16:57:37 +0530

    programming_day01

--- day-10/p20_string_reverse.js
+++
@@ -1,9 +0,0 @@
-// Reverse string without using Reverse.
-function Reverse(str) {
-  let result = "";
-  for (let firstIndex = str.length - 1; firstIndex >= 0; firstIndex--) {
-    result += str[firstIndex];
-  }
-  return result;
-}
-console.log(Reverse("deepika"));
