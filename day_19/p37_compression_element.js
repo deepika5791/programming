19c52ef59f4c1ce637eec45585e8c914954ad611
Your Name <you@example.com>
2025-05-19 19:29:24 +0530

    day_19

---
+++ day_19/p37_compression_element.js
@@ -0,0 +1,16 @@
+// Implement string compression
+function compression(str) {
+  let compressed = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      compressed += count + str[i];
+      count = 1;
+    }
+  }
+  console.log(compressed);
+}
+
+compression("aaabbccc");
